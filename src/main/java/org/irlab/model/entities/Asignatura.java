9b23e08570e577c69864bd0798ad664ca28f77e8
AntonSotoRios <114655722+AntonSotoRios@users.noreply.github.com>
2024-04-19 15:44:50 +0200

    Implementada Entidad Clase y Asignatura

--- src/main/java/org/irlab/model/entities/Asignatura.java
+++ src/main/java/org/irlab/model/entities/Asignatura.java
@@ -1,4 +1,75 @@
 package org.irlab.model.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.sql.Time;
+import java.time.LocalTime;
+
 public class Asignatura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "num_aula", nullable = false)
+    private Integer numAula;
+
+    @Column(name = "dia_semana", nullable = false)
+    private Integer diaSemana;
+
+    @Column(name = "hora_inicio", nullable = false)
+    private LocalTime horaInicio;
+
+    @Column(name = "hora_inicio", nullable = false)
+    private LocalTime horaFin;
+
+    public Asignatura(){
+
+    }
+
+    public Asignatura(Long id, String nombre, Integer numAula, Integer diaSemana,
+                      LocalTime horaInicio, LocalTime horaFin) {
+        this.id = id;
+        this.nombre = nombre;
+        this.numAula = numAula;
+        this.diaSemana = diaSemana;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre(){ return nombre;}
+
+    public void setNombre(String Nombre){ this.nombre = nombre;}
+
+    public Integer getNumAula(){ return numAula;}
+
+    public void setNumAula(Integer numAula){ this.numAula = numAula;}
+
+    public Integer getDiaSemanaa(){ return diaSemana;}
+
+    public void setDiaSemana(Integer diaSemana){ this.diaSemana = diaSemana; }
+
+    public LocalTime getHoraInicio(){return horaInicio;}
+
+    public void setHoraInicio(LocalTime horaInicio){this.horaInicio = horaInicio;}
+
+    public LocalTime getHoraFin(){return horaFin;}
+
+    public void setHoraFin(LocalTime horaFin){this.horaFin = horaFin;}
+
+
 }
--- src/main/java/org/irlab/model/entities/Clase.java
+++ src/main/java/org/irlab/model/entities/Clase.java
@@ -1,4 +1,52 @@
 package org.irlab.model.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 public class Clase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "curso", nullable = false)
+    private Long curso;
+
+    @Column(name = "grupo", nullable = false)
+    private Long grupo;
+
+    public Clase(){
+
+    }
+
+    public Clase(Long id, Long curso, Long grupo) {
+        this.id = id;
+        this.curso = curso;
+        this.grupo = grupo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Long curso) {
+        this.curso = curso;
+    }
+
+    public Long getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Long grupo) {
+        this.grupo = grupo;
+    }
 }
